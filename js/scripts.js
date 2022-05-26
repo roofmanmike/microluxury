49f46c1945b8e230149a281e8473afbcdf2e2e99
roofmanmike <62150260+roofmanmike@users.noreply.github.com>
2022-05-26 09:45:13 -0400

    na

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
 $('body').css("background-color", "gainsboro");
+
+$('#clicker').click(function(){
+  window.location.href="sierra.html";
+})
+
+$(document).ready(function () {
+    $('.dropdown-toggle').mouseover(function() {
+        $('.dropdown-menu').show();
+    })
+
+    $('.dropdown-toggle').mouseout(function() {
+        t = setTimeout(function() {
+            $('.dropdown-menu').hide();
+        }, 100);
+
+        $('.dropdown-menu').on('mouseenter', function() {
+            $('.dropdown-menu').show();
+            clearTimeout(t);
+        }).on('mouseleave', function() {
+            $('.dropdown-menu').hide();
+        })
+    })
+})
